303f0f507dd9e464313f0ed567528f1f1f350c97
mnwana <mariellenwana@gmail.com>
2022-06-20 19:21:22 -0400

    update fight function to default to fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,16 +6,27 @@ var playerMoney = 10;
 
 // // initialize enemies
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
-var enemyAttack = 12;
 
 // initiliaze fight
-var fight = function (enemyName) {
-  window.alert("Welcome to Robot Gladiators!");
-  var promptFight = window.prompt(
-    "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
-  );
-  if (promptFight.toLowerCase == "fight".toLowerCase) {
+var fight = function (enemyName, enemyHealth, enemyAttack) {
+  while (enemyHealth > 0 && playerHealth > 0) {
+    var promptFight = window.prompt(
+      "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
+    );
+    //   initiate skipping fight
+    if (promptFight == "skip" || promptFight == "SKIP") {
+      var confirmSkip = window.confirm(
+        "Are you sure you want to skp the fight? Enter 'yes' to skip."
+      );
+      if (confirmSkip) {
+        window.alert(playerName + " has chosen to skip the fight!");
+        playerMoney -= 10;
+        console.log("playerMoney", playerMoney);
+        break;
+      } else {
+        fight(enemyNames[i], enemyHealth, enemyAttack);
+      }
+    }
     //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
     enemyHealth -= playerAttack;
 
@@ -56,28 +67,21 @@ var fight = function (enemyName) {
     // check player's health
     if (playerHealth <= 0) {
       window.alert(playerName + " has died!");
+      console.log(playerName + " has died!");
+      break;
     } else {
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
   }
-  //   initiate skipping fight
-  else if (promptFight.toLowerCase == "skip".toLowerCase) {
-    var confirmSkip = window.confirm(
-      "Are you sure you want to skp the fight? Enter 'yes' to skip."
-    );
-    if (confirmSkip) {
-      window.alert(playerName + " has chosen to skip the fight!");
-      playerMoney -= 2;
-    } else {
-      fight();
-    }
-  }
-  //   prompt for accurate input
-  else {
-    window.alert("You need to choose a valid option. Try again.");
+};
+
+var main = function () {
+  window.alert("Welcome to Robot Gladiators!");
+  for (var i = 0; i < enemyNames.length; i++) {
+    var enemyHealth = 50;
+    var enemyAttack = 12;
+    fight(enemyNames[i], enemyHealth, enemyAttack);
   }
 };
 
-for(var i =0; i<enemyNames.length; i++) {
-    fight(enemyNames[i]);
-};
+main();
