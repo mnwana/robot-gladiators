eb0c5e1dcdeba0f66b4542160b07211c9f129b97
mnwana <mariellenwana@gmail.com>
2022-06-20 17:34:40 -0400

    add fight start and name capture

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("Alert, alert! Your javascript is running")
+// this asks for 
+
+var playerName = window.prompt("What's your robot's name?");
+
+// this creates a function named fight 
+
+function fight () {
+    window.alert("The fight has commenced. Good luck " + playerName + "!");
+}
+
+
+fight();
